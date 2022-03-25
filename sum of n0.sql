7f71297777af5cc966c50556358dcd252d473c97
PanjaSumalatha <102141380+PanjaSumalatha@users.noreply.github.com>
2022-03-25 22:38:37 +0530

    Add files via upload

---
+++ sum of n0.sql
@@ -0,0 +1,13 @@
+set serveroutput on;
+declare 
+    a number:=1;
+    r number:=0;
+begin
+    while a<100 loop
+        r:=r+a;
+        a:=a+1;
+    end loop;
+    dbms_output.put_line(r);
+    
+end;
+        
